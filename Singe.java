f6a9fd173532b0e3875c8e1a6dfebdde54925e45
francetr <tristanfrances@yahoo.fr>
2017-02-08 10:20:51 +0100

    Update Singe.java

--- Singe.java
+++ Singe.java
@@ -1,9 +1,14 @@
 import java.util.*;
 
 public class Singe extends Animal{
-
+    public Singe(){
+	super();
+    }
     public Singe(String chaine, int nb, char sx){
 	super(chaine, nb, sx);
     }
     
+    public void affiche(){	
+	super.affiche();
+    }
 }
