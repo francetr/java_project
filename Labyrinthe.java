75b8fcee75f74f9fd93af8899804080cea640573
francetr <tristanfrances@yahoo.fr>
2017-02-09 13:24:30 +0100

    Update Labyrinthe.java

--- Labyrinthe.java
+++ Labyrinthe.java
@@ -1,7 +1,30 @@
 import java.util.*;
-public class Labyrinthe extends Experience{
+public class Labyrinthe {
 
+    protected int temps;
+    public static int num = 0;
+    protected int numOrdre = 0;
+    
     public Labyrinthe(){
-	super();
+	num++;
+	numOrdre = num;
+	temps = "";
+    }
+    public Labyrinthe(int tps){
+	num++;
+	numOrdre = num;
+	temps = tps;
+    }
+
+    public int getTemps(){
+	return temps;
+    }
+
+    public void setTemps(int tps){
+	temps = tps;
+    }
+
+    public void affiche(){
+	System.out.println("Experience du labyrinthe" + "\nn° :  " + numOrdre);
     }
 }
