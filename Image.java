a25c16071acfe2829b73749e9e82fe2289e01bbf
francetr <tristanfrances@yahoo.fr>
2017-02-12 15:05:50 +0100

    Update Image.java

--- Image.java
+++ Image.java
@@ -1,10 +1,11 @@
+package experiences;
 import java.util.*;
 public class Image extends Experience {
     
-    protected int nbImage = 10;
     protected int nbImageChoisie = 0;
-    protected String succes = null;
-    protected int numOrdre = 0;
+    protected String succes = "";
+    protected int  numOrdre = 0;
+    protected int  essai = 0;
     public static int num = 0;
     
     public Image(){
@@ -22,6 +23,7 @@ public class Image extends Experience {
     public int getNbImageChoisie(){
 	return nbImageChoisie;
     }
+    
     public int getNumOrdre(){  // avoir le numéros de l'exp Image lors de sa création
 	return numOrdre;
     }
@@ -33,18 +35,33 @@ public class Image extends Experience {
     public String getSucces(){ // succes sous forme de String, a voir si on peut transformer en booléan
 	return succes;
     }
+
+    public int getEssai(){
+	return essai;
+    }
     
     public void setNbImageChoisie(int choix){
 	nbImageChoisie = choix;
     }
 
+    public void setSucces(String succes){
+	this.succes = succes;
+    }
+    
+    public void setEssai(int essai){
+	this.essai = essai;
+    }
+    
     public void affiche(){
-	System.out.println("expérience Image" + "\nn° " + num );
-	if (nbImageChoisie < 8){
-	    System.out.println("le sujet a trouvé la bonne image après " + nbImageChoisie + " tentatives," + " l'expérience est un " + succes + "\n" );
-	}
-	else{
-	    System.out.println("le sujet a trouvé la bonne image après " + nbImageChoisie + " tentatives," + " l'expérience est un " + succes + "\n" );
+	System.out.println("Expérience Image n°: " + numOrdre + "\n");
+	for (int i = 0; i < 5; i++){
+	    System.out.println("Essai n° : " +i );
+	    if (nbImageChoisie < 8){
+		System.out.println("Le sujet a trouvé la bonne image après " + nbImageChoisie[i] + " tentatives, l'expérience est un " + succes[i] + "\n" );
+	    }
+	    else{
+		System.out.println("Le sujet a trouvé la bonne image après " + nbImageChoisie[i] + " tentatives, l'expérience est un " + succes[i] + "\n" );
+	    }
 	}
     }
     
