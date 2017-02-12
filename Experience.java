308ca501d6afc0a4ca0bec9456902e285769185c
francetr <tristanfrances@yahoo.fr>
2017-02-12 19:36:06 +0100

    Update Experience.java

--- Experience.java
+++ Experience.java
@@ -4,14 +4,16 @@ import java.util.*;
 abstract public class Experience {
 
     public static int num = 0;
+    protected int numOrdre = 0;
     protected int nbJour = 0;
     
     public Experience(){
 	num++;
+	numOrdre = num;
     }
 
     public void affiche(){
-	System.out.println("n° Expérience : num");
+	System.out.println("n° Expérience :" + numOrdre);
     }
 
 }
