ea8f171863587e4113228c7d556027811aac4e44
francetr <tristanfrances@yahoo.fr>
2017-02-13 12:55:46 +0100

    Update Nourriture.java

--- Nourriture.java
+++ Nourriture.java
@@ -1,66 +1,50 @@
 package experiences;
 import java.util.*;
-public class Image extends Experience {
-    
-    protected int nbImageChoisie = 0;
-    protected String succes = "";
-    protected int  numOrdre = 0;
-    protected int  essai = 0;
-    public static int num = 0;
-    
-    public Image(){
-	super();
-	essai = 1;
+public class Nourriture extends Experience{
+
+    private int nbCachette = 5;
+    private int nbVisite = 0;
+    private String succes = null;
+    private int num = 0;
+    private int numImage = 0;
+	
+    public Nourriture(){
+	num ++;
+	numImage = num;
     }
 
-    public Image(String chaine, int choix){
-	super();
-	nbImageChoisie = choix;
-	succes = chaine;
-	essai = 1;
+    public int getNbVisite(){
+	return nbVisite;
     }
 
-    public int getNbImageChoisie(){
-	return nbImageChoisie;
-    }
-    
-    public int getNumOrdre(){  // avoir le numéros de l'exp Image lors de sa création
-	return numOrdre;
-    }
-
-    public int getNum(){  // avoir nb tot d'exp Image
-	return num;
-    }
-
-    public String getSucces(){ // succes sous forme de String, a voir si on peut transformer en booléan
+    public String getSucces(){
 	return succes;
     }
 
-    public int getEssai(){
-	return essai;
-    }
-    
-    public void setNbImageChoisie(int choix){
-	nbImageChoisie = choix;
+    public int getNumOrdre(){  // avoir le numéros de l'exp nourriture lors de sa création
+	return numOrdre;
     }
 
-    public void setSucces(String succes){
-	this.succes = succes;
+    public int getNum(){  // avoir nb tot d'exp nourriture
+	return num;
     }
-    
-    public void setEssai(int essai){
-	this.essai = essai;
+
+    public void setNbVisite(int v){
+	nbVisite = v;
+    }
+
+    public void setSucces(String s){
+	succes = s;
     }
     
     public void affiche(){
-	super().affiche();
-	System.out.println("Essai n° : " + essai );
-	if (nbImageChoisie < 8){
-	    System.out.println("Nombre images choisies : " + nbImageChoisie + "\n" + "Résultat : " + succes + "\n" );
+	super.affiche();
+	System.out.println("Experience de nourriture n° : " + numImage);
+	if (succes.equals("succes")){
+	    System.out.println("Nombre de visites : " + nbVisite + "\n" + "Résultat : "+ succes);
 	}
 	else{
-	    System.out.println("Nombre images choisies : " + nbImageChoisie + "\n" + "Résultat : " + succes + "\n" );
+	    System.out.println("Nombre de visittes : " + nbVisite +"Résultat : "+ succes+ "\n");
 	}
     }
 }
-    
