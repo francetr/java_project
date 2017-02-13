5fe56111a02637610ffad7d037f3fd1a4cd25df9
francetr <tristanfrances@yahoo.fr>
2017-02-13 12:55:18 +0100

    Update Image.java

--- Image.java
+++ Image.java
@@ -1,41 +1,62 @@
 package experiences;
 import java.util.*;
-public class Labyrinthe extends Experience {
-
-    protected int temps;
-    public static int num = 0;
+public class Image extends Experience {
+    
+    protected int nbImage = 0;
+    protected String succes = "";
     protected int numOrdre = 0;
-
+    protected int essai = 0;
+    public static int num = 0;
     
-    public Labyrinthe(){
+    public Image(){
 	super();
+	num++;
+	numOrdre = num;
+	essai = 1;
+    }
+
+    public int getNbImage(){
+	return nbImage;
     }
     
-    public Labyrinthe(int tps){
-	super();
-	temps = tps;
-    }
-
-    public int getTemps(){
-	return temps;
-    }
-
-    public void setTemps(int tps){
-	temps = tps;
-    }
-
-    public int getNumOrdre(){  // avoir le numéros de l'exp lab lors de sa création
+    public int getNumOrdre(){  // avoir le numéros de l'exp Image lors de sa création
 	return numOrdre;
     }
 
-    public int getNum(){  // avoir nb tot d'exp lab
+    public int getNum(){  // avoir nb tot d'exp Image
 	return num;
+    }
+
+    public String getSucces(){ // succes sous forme de String, a voir si on peut transformer en booléan
+	return succes;
+    }
+
+    public int getEssai(){
+	return essai;
+    }
+    
+    public void setNbImage(int choix){
+	nbImage = choix;
+    }
+
+    public void setSucces(String succes){
+	this.succes = succes;
+    }
+    
+    public void setEssai(int essai){
+	this.essai = essai;
     }
     
     public void affiche(){
-	super().affiche();
-	System.out.println("Experience du labyrinthe n° " + numOrdre  + "\ntemps du parcous (sec) : " + temps);
+	super.affiche();
+	System.out.println("Experience Image n° : " + numOrdre);
+	System.out.println("Essai n° : " + essai );
+	if (nbImage < 8){
+	    System.out.println("Nombre images choisies : " + nbImage + "\n" + "Résultat : " + succes + "\n" );
+	}
+	else{
+	    System.out.println("Nombre images choisies : " + nbImage + "\n" + "Résultat : " + succes + "\n" );
+	}
     }
-
-    
 }
+    
