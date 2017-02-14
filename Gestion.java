0162df95199070b8adaf3cabe23384f379e9ba51
francetr <tristanfrances@yahoo.fr>
2017-02-14 21:29:51 +0100

    Update Gestion.java

--- Gestion.java
+++ Gestion.java
@@ -13,6 +13,7 @@ public class Gestion{
 	Vector lesAnimaux = new Vector();
 	Vector lesExperiences = new Vector();
 	Vector lesCobayes = new Vector();
+	Vector lesTests = new Vector();
 	
 	Singe singe1 = new Singe("singe1", 50, 'M');
 	Singe singe2 = new Singe("singe3", 50, 'M');
@@ -39,18 +40,25 @@ public class Gestion{
         lesAnimaux.addElement(souris5); 
         lesAnimaux.addElement(souris6);
         lesAnimaux.addElement(souris7);
-
+	
 	Nourriture nour1 = new Nourriture();
 	nour1.setSucces("succes");
 	nour1.setNbVisite(4);
 	Cobaye cob1 = new Cobaye(souris3, nour1); // ajout de l'experience nour1 pour la sours3
+
 	
 	Labyrinthe lab = new Labyrinthe();
 	lab.setTemps(300);
 	Cobaye cob2 = new Cobaye(souris2, lab);
 	lesCobayes.addElement(cob1);
 	lesCobayes.addElement(cob2);
-	cob1.affiche();
+
+	Test test2 = new Test(cob2);
+	Test test1 = new Test(cob1);
+	lesTests.addElement(test1);
+	lesTests.addElement(test2);
+
+	    
 	
 	while (true){
 	    afficheMenu();
@@ -61,10 +69,8 @@ public class Gestion{
 	    case 2 : afficherAnimaux(lesAnimaux); break;
 	    case 3 : chercherAnimal(lesAnimaux); break;
 	    case 4 : modifierAnimal(lesAnimaux); break;
-	    case 5 : ajouterExperience(lesCobayes,lesAnimaux); break;       
-		// case 6 : repeterExperience(lesExperiences); break;
-	    case 7 : afficherExperience(lesCobayes); break;
-	    
+	    case 5 : ajouterExperience(lesCobayes, lesTests, lesAnimaux); break;
+	    case 6 : afficherExperience(lesCobayes, lesTests); break;
 	    }
 	}
     }
@@ -92,7 +98,6 @@ public class Gestion{
 	    Pathway path = new Pathway();
             System.out.println("Quel est groupe d'animal voulez-vous afficher?\n 1 - Souris\n 2 - Singe\n 3 - Tout");
             int rep = saisie_entier();
-            String animal = "";
             switch(rep){
 	    case 1 : path.afficherSouris(lesAnimaux); break;
 	    case 2 : path.afficherSinge(lesAnimaux); break;
@@ -170,7 +175,7 @@ public class Gestion{
 	}
     }
     
-    public static void ajouterExperience (Vector lesCobayes, Vector lesAnimaux)
+    public static void ajouterExperience (Vector lesCobayes, Vector lesTests, Vector lesAnimaux)
     /*
       Permet d'ajouter une expérience dans le vecteur contenant les expérience
     */
@@ -184,15 +189,15 @@ public class Gestion{
 	    int choix = saisie_entier();
 	    for (Enumeration e = lesAnimaux.elements(); e.hasMoreElements();){ // parcourt vecteur contenant animaux
 		for (Enumeration e2 = lesCobayes.elements(); e2.hasMoreElements();){ // parcourt vecteur contenant cobayes
-		    Cobaye item2 =(Cobaye) e2.nextElement();
 		    Animal item = (Animal) e.nextElement();
+		    Cobaye item2 =(Cobaye) e2.nextElement();
+		    
 		    if (item instanceof Souris && item.getNumOrdre() == choix && item.getId() != item2.getId()){
-			path.ajouterLabyrinthe(lesCobayes, item);
+			path.ajouterLabyrinthe(lesCobayes, lesTests, item);
 			return;
 		    }
 		}
-	    }
-	    
+	    }	    
 	}
 	else if(rep == 2){
 	    path.afficherSouris(lesAnimaux);
@@ -200,10 +205,11 @@ public class Gestion{
 	    int choix = saisie_entier();
 	    for (Enumeration e = lesAnimaux.elements(); e.hasMoreElements();){
 		for (Enumeration e2 = lesCobayes.elements(); e2.hasMoreElements();){ // parcourt vecteur contenant cobayes
-		    Cobaye item2 =(Cobaye) e2.nextElement();
 		    Animal item = (Animal) e.nextElement();
+		    Cobaye item2 =(Cobaye) e2.nextElement();
+		    
 		    if (item instanceof Souris && item.getNumOrdre() == choix && item.getId() != item2.getId()){
-			path.ajouterNourriture(lesCobayes, item);
+			path.ajouterNourriture(lesCobayes, lesTests,item);
 			return;
 		    }
 		}
@@ -214,10 +220,11 @@ public class Gestion{
 	    int choix = saisie_entier();
 	    for (Enumeration e = lesAnimaux.elements(); e.hasMoreElements();){
 		for (Enumeration e2 = lesCobayes.elements(); e2.hasMoreElements();){ // parcourt vecteur contenant cobayes
-		    Cobaye item2 =(Cobaye) e2.nextElement();
 		    Animal item = (Animal) e.nextElement();
+		    Cobaye item2 =(Cobaye) e2.nextElement();
+
 		    if (item instanceof Singe && item.getNumOrdre() == choix && item.getId() != item2.getId()){
-			path.ajouterNourriture(lesCobayes, item);
+			path.ajouterImage(lesCobayes, lesTests, item);
 			return;
 		    }
 		}
@@ -227,44 +234,7 @@ public class Gestion{
 	    return;
 	}
     }
-    /*
-    public static void repeterExperience(Vector lesExperiences){
-    \/*
-	  Permet de répéter un certain nombre de fois une expérience
-	  prends en compter un Vecteur contenant les exp
-	*\/
-	int jour = 5;
-	if (lesExperiences.size() == 0){
-	    System.out.println("Ajouter d'abord des expériences");
-	    
-	}else{
-	    Pathway path = new Pathway(); // création pathway
-	    afficherExperience(lesExperiences); //affiche experience pour choix utilisateur
-	    System.out.println("Entrer le num de l'expérience que vous voulez répéter?");
-	    int rep = saisie_entier();
-	    for (Enumeration e = lesExperiences.elements(); e.hasMoreElements();){
-		Experience item = (Experience) e.nextElement();
-		if (item.getNumOrdre() == choix){
-		    // regarde de quelle exp il s'agit 
-		    if(item instanceof Nourriture){ 
-			path.repeterNourriture(item);
-			return;
-		    }
-		    else if(item instanceof Labyrinthe){
-			path.repeterLabyrinthe(item);
-			return;
-		    }
-		    else if (item instanceof Image){
-			path.repeterImage(item);
-			return;
-		    }
-		    
-		}
-	}
-    }
-    */
-    
-    public static void afficherExperience(Vector lesCobayes){
+    public static void afficherExperience(Vector lesCobayes, Vector lesTests){
 	/*
 	  Affiche les expériences en mémoire selon choix de l'utilisateur (Nourritures/ Labyrinthes/ Image/Toutes)
 	 */
@@ -275,13 +245,24 @@ public class Gestion{
 System.out.println("Quelle expérience voulez-vous afficher?\n 1 - Labyrinthe\n 2 - Nourriture\n 3 - Image\n 4 - Toutes");
             int rep = saisie_entier();
             switch(rep){
-	    case 1 : path.afficherLabyrinthe(lesCobayes); break;
-	    case 2 : path.afficherNourriture(lesCobayes); break;
-	    case 3 : path.afficherImage(lesCobayes); break;
+	    case 1 : path.afficherLabyrinthe(lesCobayes, lesTests); break;
+	    case 2 : path.afficherNourriture(lesCobayes, lesTests); break;
+	    case 3 : path.afficherImage(lesCobayes, lesTests); break;
 	    default : 
 		for (Enumeration e = lesCobayes.elements(); e.hasMoreElements();){
-		    Cobaye cob = (Cobaye) e.nextElement();
-		    cob.affiche();
+		    for (Enumeration e2 = lesTests.elements(); e2.hasMoreElements();){
+			Cobaye cob = (Cobaye) e.nextElement();
+			Test test = (Test) e2.nextElement();
+			if (cob.getExp() instanceof Labyrinthe && cob.getId().equals(test.getId())){
+			    test.afficheLabyrinthe();
+			}
+			else if(cob.getExp() instanceof Nourriture && cob.getId().equals(test.getId())){
+			    test.afficheNourriture();
+			}
+			    else if(cob.getExp() instanceof Image && cob.getId().equals(test.getId())){
+			    test.afficheImage();
+			}
+		    }
 		}
 		break;
             }
@@ -302,8 +283,7 @@ System.out.println("Quelle expérience voulez-vous afficher?\n 1 - Labyrinthe\n 
 	System.out.println("Taper 3 pour chercher un animal");
 	System.out.println("Taper 4 pour modifier un animal");
 	System.out.println("Taper 5 pour ajouter une expérience");
-	//System.out.println("Taper 6 pour répéter une expérience");
-	System.out.println("Taper 7 pour afficher les expériences");
+	System.out.println("Taper 6 pour afficher les expériences");
     }
     
     public static String saisie_chaine ()
@@ -326,8 +306,8 @@ System.out.println("Quelle expérience voulez-vous afficher?\n 1 - Labyrinthe\n 
 	    BufferedReader buff = new BufferedReader
 		(new InputStreamReader(System.in));
 	    String chaine=buff.readLine();
-	    int num = Integer.parseInt(chaine);
-	    return num;
+	    int n = Integer.parseInt(chaine);
+	    return n;
 	}
 	catch(IOException e){return 0;}
     }
